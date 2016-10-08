500e3fe18d8edf40526783138ef91adbd732d204
AndresRQ27 <andrew.rq@gmail.com>
2016-10-08 13:10:36 -0600

    Documentacion

--- logica/Juego.java
+++ logica/Juego.java
@@ -4,12 +4,25 @@ import estructurasDatos.*;
 import jugadores.*;
 
 public class Juego {
-	private ListaSimple<Jugador> listaJugadores; //Lista para saber los jugadores que han estado en el juego
-	private Cola<Jugador>colaEspera; //Cola en caso que ya hayan 4 jugadores f√≠sicos conectados
-	private ListaSimple<Bot> botConect; //Lista de Bots activos
-	private ListaSimple<Fisico>jugadorConect; //Lista que indica cu√°les jugadores est√°n actualmente conectados
+	/**
+	 * listaJugadores Lista para saber los jugadores que han estado en el juego
+	 * colaEspera Cola en caso que ya hayan 4 jugadores fÌsicos conectados
+	 * botConect Lista de Bots activos
+	 * jugadorConect Lista que indica cu·les jugadores est·n actualmente conectados
+	 * puerto puerto donde est· el servidor
+	 * duracion duracion de la partida
+	 * getListaJugadores historial de jugadores
+	 */
+	private ListaSimple<Jugador> listaJugadores; 
+	private Cola<Jugador>colaEspera; 
+	private ListaSimple<Bot> botConect; 
+	private ListaSimple<Fisico>jugadorConect;
 	private int puerto;
 	private int duracion;
+	
+	/**
+	 * Constructor
+	 */
 	public ListaSimple<Jugador> getListaJugadores() {
 		return listaJugadores;
 	}
@@ -40,19 +53,23 @@ public class Juego {
 		this.duracion=duracion;
 		
 	}
-	//
+	/**
+	 * MÈtodo que inserta jugador en el historial
+	 */
 	public void insertarJugador(Jugador jugador){
 		listaJugadores.insertarFinal(jugador);
-					
-		
 	}
+	
 	/*
 	public void eliminarJugador(Jugador jugador){
 		listaJugadores.
 		listaJugadores.eliminar(index);(jugador);
 		
-	}
-	*/
+	}*/
+	
+	/**
+	 * 
+	 */
 	public void insertarJugadorConect(String name, String IP){
 					
 			Fisico jugadorNuevo=new Fisico();
@@ -61,6 +78,10 @@ public class Juego {
 			insertarJugadorConect(jugadorNuevo);
 		
 	}
+	
+	/**
+	 * 
+	 */
 	public void insertarJugadorConect(Fisico jugadorNuevo){
 		if (jugadorConect.tamano()<5){
 			jugadorConect.insertarFinal(jugadorNuevo);	
@@ -73,10 +94,19 @@ public class Juego {
 		
 		
 	}
+	
+	/**
+	 * MÈtodo que elimina a un jugador de la partida
+	 */
 	public void eliminarJugadorConect(Fisico jugador){
 		jugadorConect.eliminar(jugador);	
 		jugador.setActivo(false);
 	}
+	
+	/**
+	 * MÈtodo que inserta a un bot
+	 * @param nombre nombre del bot
+	 */
 	public void InsertarBotConect(String nombre){
 		Bot jugador= new Bot();
 		jugador.setNombre(nombre);
@@ -84,6 +114,10 @@ public class Juego {
 		
 	}
 	
+	/**
+	 * 
+	 * @param jugador
+	 */
 	public void insertarBotConect(Bot jugador){
 		botConect.insertarFinal(jugador);
 		jugador.setActivo(true);
