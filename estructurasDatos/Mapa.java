cba711513ca0d9f80f0c58e5fb2ead08427ad653
AndresRQ27 <andrew.rq@gmail.com>
2016-10-08 10:38:39 -0600

    Documentación e Implementaciones adicionales
    Implementaciones como los singleton de posición cero y tamaño mapa

--- estructurasDatos/Mapa.java
+++ estructurasDatos/Mapa.java
@@ -1,22 +1,39 @@
 package org.tec.tarea_programada;
-
+import org.tec.tarea_programada.PosicionCero;
+import org.tec.tarea_programada.TamanoMapa;
+/**
+ * Clase creadora del mapa de juego a base de nodos con 4 enlaces
+ * @author andre
+ *
+ * @param <T> tipo de dato del que se compondrá los nodos del mapa
+ */
+@SuppressWarnings("unchecked")
 public class Mapa <T> {
 	
-	public NodoMapa <T> cabezaPrincipal; //Puntero que se ubica en el primer nodo de la primera lista
-	public NodoMapa <T> cabezaFinal; //Puntero que se ubica en la  lista que se va a enlazar
+	/**
+	 * cabezaFinal Puntero que se ubica en la  lista que se va a enlazar
+	 */
+	public NodoMapa <T> cabezaFinal; 
 	
-
-	public Mapa(int i, int j) {
+	/**
+	 * Constructor del mapa tétramente enlazado y circular
+	 * @param y largo del mapa
+	 * @param x ancho del mapa
+	 */
+	public Mapa(int y, int x) {
+		
+		TamanoMapa.setAncho(x);
+		TamanoMapa.setAlto(y);
 		
 		NodoMapa <T> punteroActualHead = null; //Puntero que se ubica en la útlima fila enlazada
 		
-		for ( int linea = 1; linea <= i; linea ++ ) { //Crea la cantidad de listas necesarios según la cantidad de filas
+		for ( int linea = 1; linea <= y; linea ++ ) { //Crea la cantidad de listas necesarios según la cantidad de filas
 			
-			for ( int columna = 1; columna <= j; columna ++) { //Crea la cantidad de nodos necesarios según la cantidad de columnas
+			for ( int columna = 1; columna <= x; columna ++) { //Crea la cantidad de nodos necesarios según la cantidad de columnas
 				
-				NodoMapa <T> nodo = new NodoMapa <T>(null, linea, columna); //Crea un nodo nuevo para la lista
+				NodoMapa <T> nodo = new NodoMapa <T>(linea, columna); //Crea un nodo nuevo para la lista
 				
-				if (columna > 1 && columna < j) { //Para los nodos que están de por medio, los enlaza derecha e izquierda
+				if (columna > 1 && columna < x) { //Para los nodos que están de por medio, los enlaza derecha e izquierda
 					NodoMapa <T> punteroActual = this.cabezaFinal;
 					while (punteroActual.derecha != null) {
 						punteroActual = punteroActual.derecha;
@@ -27,11 +44,11 @@ public class Mapa <T> {
 				} else if (columna == 1) { //Ubica el finalHead en la cabeza de la lista que se está creando
 					this.cabezaFinal = nodo;
 					
-					if (this.cabezaPrincipal == null) { //Ubica el mainHead en la primera lista de la matriz
-						this.cabezaPrincipal = this.cabezaFinal;
+					if (PosicionCero.getCabezaPrincipal() == null) { //Ubica el mainHead en la primera lista de la matriz
+						PosicionCero.setCabezaPrincipal(this.cabezaFinal);
 					}
 					
-				} else if (columna == j ){ //Enlaza el final de la lista con el inicio de la lista para hacerl circular de left y right
+				} else if (columna == x ){ //Enlaza el final de la lista con el inicio de la lista para hacerl circular de left y right
 					NodoMapa <T> punteroActual = this.cabezaFinal;
 					while (punteroActual.derecha != null) {
 						punteroActual = punteroActual.derecha;
@@ -43,13 +60,13 @@ public class Mapa <T> {
 				}
 			}
 			
-			if (this.cabezaFinal == this.cabezaPrincipal){ //Ubica el punteroActual en la primera lista creada para tener el puntero listo cuando se quiera enlazar con la segunda lista
-				punteroActualHead = this.cabezaPrincipal;
+			if (this.cabezaFinal == PosicionCero.getCabezaPrincipal()){ //Ubica el punteroActual en la primera lista creada para tener el puntero listo cuando se quiera enlazar con la segunda lista
+				punteroActualHead = (NodoMapa<T>) PosicionCero.getCabezaPrincipal();
 
 			} else { //Enlaza los nodos de la última lista enlazada con la nueva lista creada de manera up y down
 				NodoMapa <T> nodoUnir1 = punteroActualHead;
 				NodoMapa <T> nodoUnir2 = this.cabezaFinal;
-				for (int link = 1; link <= j; link ++) {
+				for (int link = 1; link <= x; link ++) {
 					nodoUnir1.abajo = nodoUnir2;
 					nodoUnir2.arriba = nodoUnir1;
 					nodoUnir1 = nodoUnir1.derecha;
@@ -57,10 +74,10 @@ public class Mapa <T> {
 				}
 				punteroActualHead = this.cabezaFinal; //Ubica el punteroActual en la última lista enlazada para tenerlo ubicado en la lista que se tiene que enlazar cuando el finalHead cambie
 				
-				if (linea == i) { //enlaza la primera lista con la última lista los up y down para hacerla circular
-					nodoUnir1 = this.cabezaPrincipal;
+				if (linea == y) { //enlaza la primera lista con la última lista los up y down para hacerla circular
+					nodoUnir1 = (NodoMapa<T>) PosicionCero.getCabezaPrincipal();
 					nodoUnir2 = this.cabezaFinal;
-					for (int link = 1; link <= j; link ++) {
+					for (int link = 1; link <= x; link ++) {
 						nodoUnir1.arriba = nodoUnir2;
 						nodoUnir2.abajo = nodoUnir1;
 						nodoUnir1 = nodoUnir1.derecha;
@@ -69,6 +86,5 @@ public class Mapa <T> {
 				}
 			} 
 		}
-
 	}
 }
--- estructurasDatos/NodoMapa.java
+++ estructurasDatos/NodoMapa.java
@@ -1,10 +1,22 @@
-/**
- * coordenadaX: coordanada que tiene
- */
 package org.tec.tarea_programada;
 
-public class NodoMapa <T> {  //Clase principal que ubica el nodo a trabajar dentro de la matriz del juego
+/**
+ * Clase que define los nodos que compondrán el mapa del juego
+ * @author andre
+ *
+ * @param <T> tipo de dato del que se compondrá los nodos
+ */
+public class NodoMapa <T> {
 		
+	/**
+	 * dato dato que contiene el nodo
+	 * arriba enlace al nodo que está arriba
+	 * abajo enlace al nodo que está abajo
+	 * iquierda enlace al nodo que está en la izquierda
+	 * derecha enlace al nodo que está en la derecha
+	 * coordenadaY posición vertical del mapa
+	 * coordenadaX posición horizontal del mapa
+	 */
 	public T dato;
 	public NodoMapa<T> arriba;
 	public NodoMapa<T> abajo;
@@ -12,17 +24,12 @@ public class NodoMapa <T> {  //Clase principal que ubica el nodo a trabajar dent
 	public NodoMapa<T> derecha;
 	public int coordenadaY;
 	public int coordenadaX;
-	
-	public NodoMapa(T dato, int i, int j) {
-		this.dato = dato;
-		this.arriba = null;
-		this.abajo = null;
-		this.izquierda = null;
-		this.derecha = null;
-		this.coordenadaY = i;
-		this.coordenadaX = j;
-	}
-	
+
+	/**
+	 * Constructor del nodo
+	 * @param i posición hotizontal del mapa
+	 * @param j posición vertical del mapa
+	 */
 	public NodoMapa(int i, int j){
 		this.dato = null;
 		this.arriba = null;
